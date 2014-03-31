68ef7976a0e692ec194f4fa0d9a4a8dc46e0b2b2
Danny Angelo Carminati Grein <fungos@gmail.com>
2014-03-30 20:32:56 -0400

    buildall script; reapers submodule update;

---
+++ buildall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+build()
+{
+	mkdir $1
+	cd $1
+	pwd
+	qmake ../../$1/$1.pro && make
+	RET=$?
+	cd ..
+	return $RET
+}
+
+if [ -d "build" ]
+then
+	rm build -rf
+fi
+
+if [ -d "lib" ]
+then
+	rm lib -rf
+fi
+
+cp ../lib . -r
+
+git submodule update --init
+
+mkdir build
+cd build
+
+build optimalus && build reapers && build RebattreClient && build DrumLight && build Arkanoid
